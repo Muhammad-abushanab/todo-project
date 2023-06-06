e261bca43a11b5dd8f58c83a6627d8f9d53db5b1
Muhammad-abushanab <muhammad.7jehad@gmail.com>
2023-06-06 07:29:19 +0000

    JS loops

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,17 @@ let userName = window.prompt("Please type your Name :)");
 const gender = window.prompt("Please type your gender Note that you need to enter either \"male\" or \"female\"");
 const age = window.prompt("Please tell us your age ");
 
-switch (gender.toLowerCase()) {
-    case 'male':
-        userName = "Mr." + userName;
-        break;
-    case 'female':
-        userName = "Ms." + userName;
-        break;
-    default:
-        break;
+if (gender != null) {
+    switch (gender.toLowerCase()) {
+        case 'male':
+            userName = "Mr." + userName;
+            break;
+        case 'female':
+            userName = "Ms." + userName;
+            break;
+        default:
+            break;
+    }
 }
 console.log(userName);
 if (age <= 0) {
@@ -20,3 +22,34 @@ if (age <= 0) {
 if (confirm("Would like to skip the welcoming ?") == false) {
     alert(`Hello to todo web app ${userName}`)
 };
+
+
+alert('You will be asked for a 3 Yes/No questions please make sure to type \'Yes\' or \'No\' with the first letter capitalized');
+
+const questions = ['Do you like coffee ', 'Is it sunny today ', 'Do you feel okay '];
+let answers = [];
+
+function showQuestion(questions) {
+    for (let index = 0; index < questions.length; index++) {
+        let answer = prompt(questions[index]);
+        switch (answer) {
+            case 'Yes':
+                answers.push(answer);
+                break;
+            case 'No':
+                answers.push(answer);
+                break;
+            default:
+                answers.push('invalid');
+                break;
+        }
+    }
+    logAnswers(answers);
+}
+
+function logAnswers(answers) {
+    for (let index = 0; index < answers.length; index++) {
+        console.log(answers[index]);
+    }
+}
+showQuestion(questions);
